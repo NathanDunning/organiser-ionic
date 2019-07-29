a27f10e6af874eb8d9b370a6dd2f9d11c6fffe80
nathandunning <dunningn@gmail.com>
2019-07-30 00:45:43 +1200

    Created basic login and register page with simple routing between

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  {
+    path: 'register',
+    loadChildren: './register/register.module#RegisterPageModule'
   }
 ];
 
