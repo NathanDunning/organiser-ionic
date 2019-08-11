681d06da911e6254e5bbe8364a47bcb31f234257
nathandunning <dunningn@gmail.com>
2019-08-11 22:41:55 +1200

    added new headlines from google news

--- src/app/social/social.page.ts
+++ src/app/social/social.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+const newsUrl =
+  'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=b4275ca6814546c28b1fef30e93e45c4';
+
 @Component({
   selector: 'app-social',
   templateUrl: './social.page.html',
-  styleUrls: ['./social.page.scss'],
+  styleUrls: ['./social.page.scss']
 })
 export class SocialPage implements OnInit {
-
-  constructor() { }
+  articles = [];
+  constructor() {}
 
   ngOnInit() {
+    this.fetchHeadlines();
   }
 
+  async fetchHeadlines() {
+    await fetch(newsUrl)
+      .then(response => response.json())
+      .then(data => {
+        this.articles = data.articles;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    console.log(this.articles);
+  }
 }
